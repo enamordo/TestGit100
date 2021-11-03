e195ff839fb52ba7e950281453f6d76875cc0146
Atsunobu Yuge <yugechan666@gmail.com>
2021-11-04 01:00:03 +0900

    Initial Commit

---
+++ TestGit100/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestGit100
+//
+//  Created by 弓削　敦信 on 2021/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestGit100/TestGit100App.swift
@@ -0,0 +1,17 @@
+//
+//  TestGit100App.swift
+//  TestGit100
+//
+//  Created by 弓削　敦信 on 2021/11/04.
+//
+
+import SwiftUI
+
+@main
+struct TestGit100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
